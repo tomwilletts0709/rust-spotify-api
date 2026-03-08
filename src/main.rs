919e87a212e4b185af9a51fcfd8a8ff0919ff1e5
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-08 21:42:51 +0000

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE};
+use serde::{Deserialize, Serialize, };
+use std::env; 
+
+
+struct ExternalUrls {
+    spotify: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Artist {
+    name: String,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Album {
+    name: String,
+    artists: Vec<Artist>,
+    external_urls: ExternalUrls,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Track {
+    name: String,
+    href: String,
+    popularity: u32,
+    album: Album,
+    external_urls: ExternalUrls,
+}
+
+ 
+#[derive(Serialize, Deserialize, Debug)]
+struct APIResponse {
+    tracks: Items<Track>,
+
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Items<T> {
+    items: Vec<T>,
+}
+
+fn print_tracks(
+    tracks: vec<&Track>{
+
+    }
+)
+
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env.args().collect();
+    let search_query = &args[1];
+    let auth_token = &args[2];
+    
 }
